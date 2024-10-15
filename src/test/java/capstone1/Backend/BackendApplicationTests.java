9b62b4eb1bd32fd8dbbe2d08044553091b8474f2
Meowning <118808242+Meowning@users.noreply.github.com>
2024-10-15 22:26:00 +0900

    initial commit

---
+++ src/test/java/capstone1/Backend/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+package capstone1.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
